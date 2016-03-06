672e4bedd8325d01cbba5a22ef82fde69a8e7a51
Maxime CARLIER <maxime.mca@gmail.com>
2016-03-06 19:39:58 +0100

    Add constructor for GroundStrategey, and implement it in HuntedBiome

--- src/main/java/fr/unice/polytech/qgl/qda/strategy/GroundPhase/GroundStrategy.java
+++ src/main/java/fr/unice/polytech/qgl/qda/strategy/GroundPhase/GroundStrategy.java
@@ -1,11 +1,20 @@
 package fr.unice.polytech.qgl.qda.strategy.GroundPhase;
 
+import fr.unice.polytech.qgl.qda.Game.Assignment;
+import fr.unice.polytech.qgl.qda.Island.IslandMap;
+import fr.unice.polytech.qgl.qda.Json.actions.Action;
 import fr.unice.polytech.qgl.qda.strategy.Strategy;
 import org.json.JSONObject;
+
+import java.util.LinkedList;
 
 public class GroundStrategy extends Strategy {
     public GroundStrategy(JSONObject context) {
         super(context);
+    }
+
+    public GroundStrategy(IslandMap islandMap, Assignment assignment, LinkedList<JSONObject> bufferActions, LinkedList<Action> actionsHistory) {
+        super(islandMap, assignment, bufferActions, actionsHistory);
     }
 
     @Override
--- src/main/java/fr/unice/polytech/qgl/qda/strategy/GroundPhase/SpecificPattern/HuntedBiome.java
+++ src/main/java/fr/unice/polytech/qgl/qda/strategy/GroundPhase/SpecificPattern/HuntedBiome.java
@@ -1,10 +1,15 @@
 package fr.unice.polytech.qgl.qda.strategy.GroundPhase.SpecificPattern;
 
+import fr.unice.polytech.qgl.qda.Game.Assignment;
 import fr.unice.polytech.qgl.qda.Island.Biome;
+import fr.unice.polytech.qgl.qda.Island.IslandMap;
 import fr.unice.polytech.qgl.qda.Island.Ressource;
 import fr.unice.polytech.qgl.qda.Island.Tile;
+import fr.unice.polytech.qgl.qda.Json.actions.Action;
 import fr.unice.polytech.qgl.qda.strategy.GroundPhase.GroundStrategy;
 import org.json.JSONObject;
+
+import java.util.LinkedList;
 
 public class HuntedBiome extends GroundStrategy {
 
@@ -12,17 +17,22 @@ public class HuntedBiome extends GroundStrategy {
     private Ressource targetedResource;
     private Biome biomeOfResource;
 
+
+
     /**
      *
      * Default constructor
      *
-     * @param context The current context in which we create the Strategy
+     * @param islandMap The current map running
+     * @param assignment The assigments of the map
+     * @param bufferActions The actions waiting to be sent
+     * @param actionsHistory The previous actions
      * @param currentTile The first Tile we want to reach
      * @param targetedResource The resource we want to harvest
      * @param biomeOfResource The biome of the target Tile & Resource
      */
-    public HuntedBiome(JSONObject context, Tile currentTile, Ressource targetedResource, Biome biomeOfResource) {
-        super(context);
+    public HuntedBiome(IslandMap islandMap, Assignment assignment, LinkedList<JSONObject> bufferActions, LinkedList<Action> actionsHistory, Tile currentTile, Ressource targetedResource, Biome biomeOfResource) {
+        super(islandMap, assignment, bufferActions, actionsHistory);
         this.currentTile = currentTile;
         this.targetedResource = targetedResource;
         this.biomeOfResource = biomeOfResource;
